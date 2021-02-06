f7cdbde361f4240806b23626a6ab7dc171009a4e
Yash Kadam <yashkadam278@gmail.com>
2021-02-06 10:22:08 +0530

    homepage Changes

--- public/index.js
+++ public/index.js
@@ -55,3 +55,19 @@ $("#example14").calendar({
   inline: true,
 });
 $("#example15").calendar();
+
+//Get the button:
+mybutton = document.getElementById("backTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
